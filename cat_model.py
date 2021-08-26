ee191743c56281d5da8b8348682cbbd7cfe21ed5
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-26 11:09:44 +0200

    Update cat_model.py

--- cat_model.py
+++ cat_model.py
@@ -1,1 +1,8 @@
+import joblib
 
+class CatBoostRegressor():
+  def __init__(self):
+        self.model = joblib.load('cat_model.pkl')
+
+    def predict_price(self, data):
+        return self.model.predict(data)
