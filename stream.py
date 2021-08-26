0b574a187c87fb22e266a5fa2b357f69968fcb0a
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-26 11:57:18 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime
+import datetime
 from cat_model import CatBoostRegressor
 from scaler import RobustScaler
 
