d5d75719d4b7c407c60447e6bdb09ba7ccc9795a
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-26 09:40:26 +0200

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-class LightGBM():
-    def __init__(self):
-        self.model = joblib.load('light_model.pkl')
-
-    def predict_price(self, data):
-        return self.model.predict(data)
-        
