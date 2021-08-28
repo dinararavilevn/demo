15b9c93d63aa1a121f5c02de024312ed326b995f
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-28 08:33:27 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,8 @@ import joblib
 
 class LightGBM():
     def __init__(self):
-        self.model = joblib.load('light_model.pkl')
+        #self.model = joblib.load('light_model.pkl')
+        self.model = joblib.load('lgbm.pkl')
 
     def predict_price(self, data):
         return self.model.predict(data)
