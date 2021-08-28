598a267b574b4115857b48b888bd1f041665a613
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-28 13:59:11 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -77,8 +77,11 @@ df_with_cities_coo['day_delta'] = (now - first_date).days
 df_with_cities_coo['hour'] = now.hour
 df_with_cities_coo['year'] = now.year
 df_with_cities_coo = add_feature(df_with_cities_coo)
-df_with_cities_coo['expensive_region'] = lambda x: 0 if x>0 else 0 if x ==0 else -1
-
+if df_with_cities_coo['city']=='Севастополь' or df_with_cities_coo['city']=='Москва' or df_with_cities_coo['city']=='Санкт-Петербург':
+    df_with_cities_coo['expensive_region'] = 1
+else:
+    df_with_cities_coo['expensive_region'] = 0
+st.write(df_with_cities_coo)
 #Нормализуем числовые признаки
 #nums = df_with_cities_coo.drop(['object_type', 'building_type'], axis=1) 
 #scaler = RobustScaler()
