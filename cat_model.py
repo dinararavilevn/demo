c5cc2a01602a138c28b0df0d135a55b5450e36e6
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-28 08:30:49 +0200

    Delete cat_model.py

--- cat_model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-class CatBoostRegressor():
-  def __init__(self):
-      self.model = joblib.load('cat_model.pkl')
-
-  def predict_price(self, data):
-      return self.model.predict(data)
