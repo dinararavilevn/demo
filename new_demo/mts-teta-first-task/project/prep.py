a9778de4fb8dd78d7c5f3ab073d93700d5ea6ea9
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-29 19:25:26 +0300

    Add files via upload

--- new_demo/mts-teta-first-task/project/prep.py
+++ new_demo/mts-teta-first-task/project/prep.py
@@ -1,53 +1,162 @@
-import os
-import numpy as np
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 import pickle
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler, RobustScaler
+import os
+import yaml
+
+
+def read_yaml(path):
+    dir_path = os.path.dirname(os.path.dirname(os.getcwd()))
+    # dir_path = os.path.join(dir_path, 'config')
+    return yaml.safe_load(open(os.path.join(dir_path, path)))['project']
 
 
 def read_data(path):
     return pd.read_csv(path)
 
 
-def clean_data(data):
-    data = data.query('price > 100000')
-    data = data.query('price < 500000000')
-    data = data.query('rooms != -2')
-    data.index = np.arange(data.shape[0])
+def prep_date(data):
+    data["date_time"] = data["date"] + " " + data["time"]
+    data = data.drop(["date", "time"], axis=1)
+    data['date_time'] = pd.to_datetime(data["date_time"])
+    data["day_delta"] = (data["date_time"] - data.date_time.min()).dt.days
+    # data = data.drop("date_time", axis=1)
     return data
 
 
-def prep_date(data):
-    data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-    data = data.sort_values(by='date')
-    data['year'] = data['date'].dt.year
-    data = data.drop(["date", "time"], axis=1)
+def clean_data(data):
+    data = data[(data.price > 0) & (data.rooms > 0) & (data.area > 0) & (data.kitchen_area > 0)]
+    data = data.drop_duplicates()
+    data.reset_index(inplace=True, drop=True)
+    return data
+
+
+def drop_regions(data, amount_of_regions):
+    regions_to_drop = data.groupby('region')['region'].count().sort_values().head(amount_of_regions).index.to_list()
+    data = data[~data['region'].isin(regions_to_drop)]
+    return data
+
+
+def filter_by_region(data, amount_of_std, feature):
+    def clean(reg):
+        std_shift = data.loc[data.region == reg][feature].std() * amount_of_std
+        feat_mean = data.loc[data.region == reg][feature].mean()
+        lower_bound = feat_mean - std_shift
+        upper_bound = feat_mean + std_shift
+        rows_to_drop = np.where((data.loc[data.region == reg, feature] < lower_bound) | \
+                                (data.loc[data.region == reg, feature] >= upper_bound))[0]
+        indexes = data.loc[data.region == reg].iloc[rows_to_drop].index
+        data.drop(indexes, axis=0, inplace=True)
+        data.reset_index(inplace=True, drop=True)
+        return data
+
+    for region in tqdm(data.region.unique()):
+        data = clean(region)
+    return data
+
+
+def convert_object_type(data):
+    data["object_type"] = data["object_type"].map({1: 0, 11: 1}).astype(int)
+    return data
+
+
+def make_time_features(data):
+    data["hour"] = data['date_time'].apply(lambda x: x.hour)
+    data["year"] = data["date_time"].apply(lambda x: x.year)
     return data
 
 
 def add_feature(data):
-    def calc_mean_room_area():
+    def calc_mean_room_area(data):
         return (data['area'] - data['kitchen_area']) / (abs(data['rooms']))
 
-    data['mean_room_area'] = calc_mean_room_area()
+    data['mean_room_area'] = calc_mean_room_area(data)
     data['percent_of_kitchen_area'] = data['kitchen_area'] / data['area']
     data['percent_of_level'] = data['level'] / data['levels']
     return data
 
 
-def label_transformer(data):
-    categoricals = ['building_type', 'object_type', 'region', 'year']
-    for col in categoricals:
-        le = LabelEncoder()
-        le.fit(data[col])
-        data[col] = le.transform(data[col])
-        with open(f"encoder/label_{col}.pkl", "wb") as file:
-            pickle.dump(le, file)
+def split_data(data, train_size):
+    data = data.sort_values(by="date_time")
+    train_df = data.iloc[:round(data.shape[0] * train_size)]
+    test_df = data.iloc[round(data.shape[0] * train_size):]
+    return train_df, test_df
+
+
+def reduce_mem_usage(data, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = data.memory_usage().sum() / 1024 ** 2
+    for col in data.columns:
+        col_type = data[col].dtypes
+        if col_type in numerics:
+            c_min = data[col].min()
+            c_max = data[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    data.loc[:, col] = data.loc[:, col].astype(np.float32)
+                else:
+                    data.loc[:, col] = data.loc[:, col].astype(np.float64)
+    end_mem = data.memory_usage().sum() / 1024 ** 2
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem,
+                                                                              100 * (start_mem - end_mem) / start_mem))
     return data
 
 
-def split_data(data):
-    train = data.iloc[data.index[:-100000]]
-    test = data.iloc[data.index[-100000:]]
-    return train, test
+def scale_data(X, scaler_algo, path):
+    if scaler_algo == "standart":
+        scaler = StandardScaler()
+        scaler.fit(X)
+    elif scaler_algo == "robust":
+        scaler = RobustScaler()
+        scaler.fit(X)
+    else:
+        print("enter scaler name")
+
+    with open(path, "wb") as file:
+        pickle.dump(scaler, file)
+
+
+def save_data(data, path):
+    data.to_csv(path, index=False)
+
+
+if __name__ == "__main__":
+    config = read_yaml("config.yml")
+    project_path = config["project_path"]
+    n_regions = config['preprocessing']['amount_of_regions']
+    n_std = config['preprocessing']['amount_of_std']
+    scaler = config['preprocessing']["scaler"]
+
+    df = read_data(os.path.join(project_path, *["data", "raw", "all_v2.csv"]))
+    df = prep_date(df)
+    df = clean_data(df)
+    df = drop_regions(df, n_regions)
+    df = filter_by_region(df, n_std, "price")
+    df = filter_by_region(df, n_std, "area")
+    df = make_time_features(df)
+    df = add_feature(df)
+    df = convert_object_type(df)
+
+    train, test = split_data(df, config['preprocessing']['train_size'])
+    # train = reduce_mem_usage(train)
+    # test = reduce_mem_usage(test)
+
+    save_data(train, os.path.join(project_path, *["data", "clean", f"train_{n_regions}_{n_std}.csv"]))
+    save_data(test, os.path.join(project_path, *["data", "clean", f"test_{n_regions}_{n_std}.csv"]))
+
+    scale_data(train.drop(["price", "date_time", 'region'], axis=1), scaler,
+               f"scalers/{scaler}_{n_regions}_{n_std}.pkl")
--- new_demo/mts-teta-first-task/project/train.py
+++ new_demo/mts-teta-first-task/project/train.py
@@ -1,16 +1,53 @@
-import os
-import numpy as np
 import pandas as pd
-import pickle
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.metrics import mean_absolute_error
-
+import numpy as np
+import os
+import mlflow
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
+import catboost as cat
 import lightgbm as lgbm
+from prep import read_yaml, read_data
+import pickle
 
-from prep import read_data, clean_data,prep_date, add_feature, label_transformer, split_data
+
+def linear(X, y, test, exp_id, run_name):
+    run_id = mlflow.start_run(
+        experiment_id=exp_id.experiment_id,
+        run_name=run_name)
+    linear = LinearRegression(normalize=True)
+    linear.fit(X, y)
+    pred = linear.predict(test.drop("price", axis=1))
+    client.log_metric(run_id.info.run_id, "MAE", round(mean_absolute_error(test.price, pred)))
+    client.log_metric(run_id.info.run_id, "r2", r2_score(test.price, pred))
+    params = linear.get_params()
+    client.log_param(run_id.info.run_id, "normalize", "True")
+    # client.log_artifact(run_id.info.run_id,local_path='test.ipynb',
+    #                            artifact_path='code')
+    # mlflow.sklearn.log_model(linear,
+    #                                 artifact_path="model",
+    #                                 registered_model_name="mlruns/models/linear")
+    mlflow.end_run()
 
 
-def train_lgbm(X_train, y_train, X_test, y_test):
+def tree():
+    pass
+
+
+def forest():
+    pass
+
+
+def light():
+    pass
+
+
+def catboost():
+    pass
+
+
+def train_light(X_train, y_train, X_test, y_test, scaler_name):
+    with open(f"scalers/{scaler_name}.pkl", 'rb') as file:
+        scaler = pickle.load(file)
     params = {'bagging_fraction': 0.9615087447098526,
               'bagging_freq': 0,
               'boosting_type': 'gbdt',
@@ -26,31 +63,45 @@ def train_lgbm(X_train, y_train, X_test, y_test):
               'min_sum_hessian_in_leaf': None,
               'num_leaves': 152,
               'objective': 'regression',
-              'seed': 42,
+              'seed': 123,
               'subsample_for_bin': 3500,
               'verbose': -1,
               'n_estimators': 5000}
+    X_train = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns)
+    X_test = pd.DataFrame(scaler.transform(X_test), columns=X_train.columns)
     train_dataset = lgbm.Dataset(X_train, y_train,
-                                 categorical_feature=['building_type', 'object_type', 'region', 'year'])
+                                 categorical_feature=['building_type', 'object_type', 'year', 'hour'])
     test_dataset = lgbm.Dataset(X_test, y_test,
-                                categorical_feature=['building_type', 'object_type', 'region', 'year'])
+                                categorical_feature=['building_type', 'object_type', 'year', 'hour'])
     model = lgbm.train(params=params,
                        train_set=train_dataset,
                        valid_sets=[train_dataset, test_dataset],
                        num_boost_round=20000,
                        early_stopping_rounds=50,
                        verbose_eval=100)
-    with open("model/lgbm.pkl", "wb") as file:
+    with open("models/lgbm.pkl", 'wb') as file:
         pickle.dump(model, file)
 
 
 if __name__ == "__main__":
-    df = read_data("data/all_v2.csv")
-    df = clean_data(df)
-    df = add_feature(df)
-    df = prep_date(df)
-    df = label_transformer(df)
-    print(df.columns)
-    #train, test = split_data(df)
-    #train_lgbm(train.drop("price", axis=1), train.price,
-    #           test.drop("price", axis=1), test.price)
+    config = read_yaml("config.yml")
+    project_path = config["project_path"]
+
+    mlflow.set_registry_uri("sqlite:///mlruns/mlflow.db")
+    mlflow.set_tracking_uri("sqlite:///mlruns/mlflow.db")
+    client = mlflow.tracking.MlflowClient(tracking_uri="sqlite:///mlruns/mlflow.db")
+
+    try:
+        exp_id = client.create_experiment("test")
+    except mlflow.exceptions.MlflowException:
+        exp_id = client.get_experiment_by_name("test")
+
+    train = read_data(os.path.join(project_path, *["data", "clean", "train_40_3.csv"]))
+    test = read_data(os.path.join(project_path, *["data", "clean", "test_40_3.csv"]))
+    train = train.drop(["date_time", "region"], axis=1)
+    test = test.drop(["date_time", "region"], axis=1)
+    y = train.price
+    X = train.drop(["price"], axis=1)
+
+    # linear(X, y, test, exp_id, "first try")
+    train_light(X, y, test.drop("price", axis=1), test.price, config["stream"]["scaler_name"])
