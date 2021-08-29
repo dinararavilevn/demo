9f13117d01984857f4e6746817059e04b3649e1c
Dinara_Oz <63777919+dinararavilevn@users.noreply.github.com>
2021-08-29 18:48:50 +0300

    Delete scaler.py

--- scaler.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-class RobustScaler():
-    def __init__(self):
-        self.scaler = joblib.load('scaler.pkl')
-
-    def get_scaled_data(self, nums):
-        return self.scaler.transform(nums)
-        
